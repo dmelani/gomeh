3e421065c37706df7c993c8f38edfd089221397c
Daniel Melani <daniel.melani@gmail.com>
2015-03-01 21:38:11 +0100

    WIP

--- gomeh.go
+++ gomeh.go
@@ -6,6 +6,18 @@ import (
 	"fmt"
 	"log"
 	"encoding/json"
+	"runtime"
+	"strings"
+	"errors"
+
+	"github.com/go-gl/gl/v4.1-core/gl"
+	"github.com/go-gl/glfw/v3.1/glfw"
+	"github.com/go-gl/mathgl/mgl32"
+)
+
+const (
+	WindowWidth = 800
+	WindowHeight = 600
 )
 
 type pointsAndBoxes struct {
@@ -34,8 +46,131 @@ func main() {
 		fmt.Printf("usage: %s <filename>\n", os.Args[0])
 		os.Exit(1)
 	}
-
 	d := loadJson(os.Args[1])
 
 	fmt.Println(d.Type.Point)
+
+	runtime.LockOSThread()
+	if err := glfw.Init(); err != nil {
+		log.Fatalln("Failed to initialize glfw:", err)
+	}
+	defer glfw.Terminate()
+
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+	window, err := glfw.CreateWindow(WindowWidth, WindowHeight, "Cube", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+	window.MakeContextCurrent()
+
+	if err := gl.Init(); err != nil {
+		log.Fatal("Failed to initialize opengl")
+	}
+
+	version := gl.GoStr(gl.GetString(gl.VERSION))
+	fmt.Println("OpenGL version", version)
+
+	program, err := newProgram(vertexShader, fragmentShader)
+	if err != nil {
+		log.Fatal(err)
+	}
+	gl.UseProgram(program)
+
+	projection := mgl32.Perspective(mgl32.DegToRad(45.0), float32(WindowWidth)/WindowHeight, 0.1, 10.0)
+	projectionUniform := gl.GetUniformLocation(program, gl.Str("projection\x00"))
+	gl.UniformMatrix4fv(projectionUniform, 1, false, &projection[0])
+
+	camera := mgl32.LookAtV(mgl32.Vec3{3, 3, 3}, mgl32.Vec3{0, 0, 0}, mgl32.Vec3{0, 1, 0})
+	cameraUniform := gl.GetUniformLocation(program, gl.Str("camera\x00"))
+	gl.UniformMatrix4fv(cameraUniform, 1, false, &camera[0])
+
+	model := mgl32.Ident4()
+	modelUniform := gl.GetUniformLocation(program, gl.Str("model\x00"))
+	gl.UniformMatrix4fv(modelUniform, 1, false, &model[0])
+
+	gl.ClearColor(0.0, 0.0, 0.0, 0.0)
+	for !window.ShouldClose() {
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
 }
+func newProgram(vertexShaderSource, fragmentShaderSource string) (uint32, error) {
+	vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
+	if err != nil {
+		return 0, err
+	}
+
+	fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
+	if err != nil {
+		return 0, err
+	}
+
+	program := gl.CreateProgram()
+
+	gl.AttachShader(program, vertexShader)
+	gl.AttachShader(program, fragmentShader)
+	gl.LinkProgram(program)
+
+	var status int32
+	gl.GetProgramiv(program, gl.LINK_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetProgramiv(program, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetProgramInfoLog(program, logLength, nil, gl.Str(log))
+
+		return 0, errors.New(fmt.Sprintf("failed to link program: %v", log))
+	}
+
+	gl.DeleteShader(vertexShader)
+	gl.DeleteShader(fragmentShader)
+
+	return program, nil
+}
+
+func compileShader(source string, shaderType uint32) (uint32, error) {
+	shader := gl.CreateShader(shaderType)
+
+	csource := gl.Str(source)
+	gl.ShaderSource(shader, 1, &csource, nil)
+	gl.CompileShader(shader)
+
+	var status int32
+	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to compile %v: %v", source, log)
+	}
+
+	return shader, nil
+}
+
+var vertexShader string = `
+#version 330
+uniform mat4 projection;
+uniform mat4 camera;
+uniform mat4 model;
+in vec3 vert;
+void main() {
+    gl_Position = projection * camera * model * vec4(vert, 1);
+}
+` + "\x00"
+
+var fragmentShader = `
+#version 330
+out vec4 outputColor;
+void main() {
+    outputColor = vec4(1.0, 1.0, 1.0, 1.0);
+}
+` + "\x00"
